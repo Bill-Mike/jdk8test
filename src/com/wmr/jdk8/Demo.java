69e6c57e0a592038ec65cf9f8f2a105b8007de2c
wmr123456 <844161654@qq.com>
2019-10-09 14:20:32 +0800

    init

--- src/com/wmr/jdk8/Demo.java
+++ src/com/wmr/jdk8/Demo.java
@@ -7,5 +7,22 @@ public class Demo {
 
     public static void main(String[] args) {
         show(new MyFunctionalInterfaceImpl());
+
+        //调用show方法，方法的参数是一个接口的匿名内部类
+        show(new MyFunctionalInterface() {
+            @Override
+            public void method() {
+                System.out.println("使用匿名内部类");
+            }
+
+        });
+
+        //调用show方法，方法的参数是一个参数是接口，所以我们可以使用lambda表达式
+        show(() ->{
+            System.out.println("使用Lambda表达式");
+        });
+
+        //简化Lambda表达式
+        show(() -> System.out.println("使用Lambda表达式"));
     }
 }
